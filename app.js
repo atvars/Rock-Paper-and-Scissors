af0e9bd7dce1f95f2fd5762ccd20cfd5aec451bd
atvars <avelavs@gmail.com>
2024-05-23 11:11:55 +0000

    added user choise

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOISE = ROCK;
+
+let gameIsRunning = false;
+
+// Promt for user to enter his choise 
+const getPlayerChoise = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER}, or ${SCISSORS}?`,
+    ''
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOISE} for you!`);
+    return DEFAULT_USER_CHOISE;
+  }
+  return selection;
+};
+
+startGameBtn.addEventListener('click', function () {
+  if (gameIsRunning) {
+    return
+  }
+  gameIsRunning = false;
+  console.log('Game is starting');
+  const playerSelection = getPlayerChoise();
+  console.log(playerSelection);
+});
